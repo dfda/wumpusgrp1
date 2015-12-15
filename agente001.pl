c95b5f886e270f52b98f07a9d3fcb6dd6ff301f4
Alesson Renato Lopes Valenca <alessonrenato@gmail.com>
2015-12-15 14:28:43 -0300

    Casas suspeitas como fato dinâmico

--- agente001.pl
+++ agente001.pl
@@ -32,7 +32,7 @@
 % ?- start.
 
 :- load_files([wumpus3]).
-:- dynamic ([agent_flecha/1, wumpus/1, ouro/1, minhacasa/1, orientacao/1, casas_seguras/1, casas_visitadas/1, casa_anterior/1, casa_da_frente/1]). %fatos dinamicos
+:- dynamic ([agent_flecha/1, wumpus/1, ouro/1, minhacasa/1, orientacao/1, casas_seguras/1, casas_visitadas/1, casa_anterior/1, casa_da_frente/1, casas_suspeitas/1]). %fatos dinamicos
 
 wumpusworld(pit3, 4).
 
@@ -55,7 +55,9 @@ init_agent :-                       % se nao tiver nada para fazer aqui, simples
     retractall(casa_anterior(_)),
     assert(casa_anterior([1,1])),
     retractall(casa_da_frente([_,_])),
-    assert(casa_da_frente([2,1])).
+    assert(casa_da_frente([2,1])),
+    retractall(casas_suspeitas(_)),
+    assert(casas_suspeitas(0)).
 
 
 restart_agent :- 

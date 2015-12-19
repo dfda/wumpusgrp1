1b8e12761fe30c23f1c993fb6c882621d0985f48
Cassia Regina Franca Barbosa <caassiaregina@live.com>
2015-12-19 16:56:06 -0300

    calculacao sentido 270'

--- agente001.pl
+++ agente001.pl
@@ -413,6 +413,28 @@ calculacao([X, Y1], 270, [X, Y2], turnleft):-
     Y2>Y1,
     novosentidoleft.
 
+calculacao([X1, Y1], 270, [X2, Y2], goforward):-
+    X1>X2,
+    Y1>Y2,
+    faz_casa_anterior,
+    novaposicao.
+
+calculacao([X1, Y1], 270, [X2, Y2], goforward):-
+    X1<X2,
+    Y1>Y2,
+    faz_casa_anterior,
+    novaposicao.
+
+calculacao([X1, Y1], 270, [X2, Y2], turnleft):-
+    X1<X2,
+    Y1<Y2,
+    novosentidoleft.
+
+calculacao([X1, Y1], 270, [X2, Y2], turnright):-
+    X1>X2,
+    Y1<Y2,
+    novosentidoright.
+
 tiro :-  %agente com flecha e capaz de atirar no wumpus e flecha e decrementada%
     agente_flecha(X),
     X>0,
